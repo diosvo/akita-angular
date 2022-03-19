68294ff9ddfd4297987e4d61733a65445448e49b
Dios Vo <vtmn1212@gmail.com>
2022-03-19 18:50:47 +0700

    core: install taiga-ui packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NG_ENTITY_SERVICE_CONFIG } from '@datorama/akita-ng-entity-service';
 import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { iconsPathFactory, TuiRootModule, TUI_ICONS_PATH } from '@taiga-ui/core';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,15 +19,22 @@ export const baseUrl = environment.URL;
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    AkitaNgRouterStoreModule,
+
     environment.production
       ? []
       : AkitaNgDevtools.forRoot(),
+
+    TuiRootModule,
+    AkitaNgRouterStoreModule,
   ],
   providers: [
     {
       provide: NG_ENTITY_SERVICE_CONFIG,
       useValue: { baseUrl }
+    },
+    {
+      provide: TUI_ICONS_PATH,
+      useValue: iconsPathFactory('assets/taiga-ui/icons/')
     }
   ],
   bootstrap: [AppComponent]
